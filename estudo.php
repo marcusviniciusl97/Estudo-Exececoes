670ce80c6551457734c85c5cbb350719f0c29d43
Marcus <marcusviniciusrocha97@gmail.com>
2024-11-21 20:21:34 -0300

    Mais anotações

--- estudo.php
+++ estudo.php
@@ -1,10 +1,10 @@
 <?php
 
-class contaBancaria // Foi definida a Classe que será utilizada para realizar o cadastro da conta bancária.
+class contaBancaria // Foi definida a Classe que será utilizada para modelar uma conta bancária.
 {
 
     private $banco;
-    private $nomeTitular; // Definição dos objetos.
+    private $nomeTitular; // Definição dos atributos (propriedades) da classe.
     private $numeroConta;
     private $agenciaConta;
     private $saldo;
@@ -19,24 +19,25 @@ class contaBancaria // Foi definida a Classe que será utilizada para realizar o
         $this->saldo = $saldo;
     }
 
-    public function obterSaldo(){   //utilização da função para saber o saldo vindo do $conta = new contaBnacaria.
+    public function obterSaldo(){   /// Função que retorna o saldo atual da conta.
 
         return 'Seu saldo é de R$ ' . $this->saldo;
     }
 
-    public function depositar($valor){ //Função 1
+    public function depositar($valor){ //Função 1. Função que retorna o valor depositado.
 
         $this->saldo += $valor;
-        return 'Deposito de R$ '. $valor;
+        return 'Foi feito um Deposito de R$ '. $valor;
     }
 
-    public function sacar($valor){ //Função 2
+    public function sacar($valor){ //Função 2. Função que retorna o valor sacado.
 
         $this->saldo -= $valor;
-        return 'Saque de R$ '. $valor;
+        return 'Foi feito um Saque de R$ '. $valor;
     }
     
 } // Lembrar que o escopo deve ser fechado da classe mantendo o que é public e private.
+
 
 $conta = new contaBancaria( //Seguindo a ordem no qual foi determinada os objetos da classe no function __contruct.
 
@@ -46,21 +47,23 @@ $conta = new contaBancaria( //Seguindo a ordem no qual foi determinada os objeto
     '8504',
     0    
 );
+
+var_dump($conta); // imprimir detalhes da variável.
     
     echo $conta->obterSaldo(); // Imprimir o saldo da conta atual.
 
     echo PHP_EOL; // Utilizado para pular uma linha.
 
-    $conta->depositar(500.00); // Valor dado a variável $valor na função depositar, montada dentro do escopo da classe. Função 1.
+    echo $conta->depositar(500.00); // Valor dado a variável $valor na função depositar, montada dentro do escopo da classe. Função 1.
 
     echo PHP_EOL;
 
-    echo $conta->obterSaldo(); // Imprimir o saldo da conta atual. Está sendo utilizado o echo pois foi dado como return da function apenas a string. Function 1 e 2.
+    echo $conta->obterSaldo(); // Imprimir o saldo da conta atual. 
 
     echo PHP_EOL;
 
-    $conta->sacar(150.00); // Valor dado a variável $valor na função sacar, montada dentro do escopo da classe. Função 2. JS = conta.sacar() PHP = $conta->sacar()
-
+    echo $conta->sacar(150.00); // Valor dado a variável $valor na função sacar, montada dentro do escopo da classe. Função 2. JS = conta.sacar() PHP = $conta->sacar(). 
+                                //Está sendo utilizado o echo pois foi dado como return da function apenas a string. Function 1 e 2.
     echo PHP_EOL;
 
     echo $conta->obterSaldo();
